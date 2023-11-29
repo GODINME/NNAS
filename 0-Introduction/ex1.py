3e60927ffd6a3bee8ac84e5e3f6c116963ed4789
GODINME <poffei@st.ug.edu.gh>
2023-11-29 22:13:09 +0000

    single neuron with 4 inputs

--- 0-Introduction/ex1.py
+++ 0-Introduction/ex1.py
@@ -1,9 +1,9 @@
 # Single Neural Network with 3 Inputs and its associated random weights and bias
-inputs = [1, 2, 3]
-weights = [0.2, 0.8, -0.5]
-bias = 2
+inputs = [1.0, 2.0, 3.0, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2.0
 
-output = (inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias)
+output = (inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + inputs[3] * weights[3] + bias)
 
 print(output)
 
