491d61482cdcb447b611d84cd703f4abdbf7cc60
GODINME <poffei@st.ug.edu.gh>
2023-11-29 22:07:26 +0000

    single neuron with 3 inputs

---
+++ 0-Introduction/ex1.py
@@ -0,0 +1,9 @@
+# Single Neural Network with 3 Inputs and its associated random weights and bias
+inputs = [1, 2, 3]
+weights = [0.2, 0.8, -0.5]
+bias = 2
+
+output = (inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias)
+
+print(output)
+
